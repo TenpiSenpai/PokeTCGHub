dac2c645a4766fed06a07a5d77a2d5b940ab6a0b
Tenpi <TenpiSenpai@proton.me>
2025-12-12 16:18:09 -0500

    start deck 100 cards

---
+++ app/pages/sets/jp/assorted_jp.vue
@@ -0,0 +1,7 @@
+<template>
+    <h2 class="text-2xl p-8 border-b">Assorted Unreleased Japanese Pokemon Cards</h2>
+    <div class="p-8">
+        <h3 class="text-2xl">Assorted Promotional Cards</h3>
+    </div>
+    <card-list set="jp_promo" :excludeTypes="true" />
+</template>
