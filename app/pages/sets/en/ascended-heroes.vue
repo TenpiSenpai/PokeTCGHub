b5ad9ed32eac301c07807afc4b989adafde66e6e
Tenpi <TenpiSenpai@proton.me>
2026-01-26 07:09:23 -0500

    ascended heroes full set

--- app/pages/sets/en/ascended-heroes.vue
+++ app/pages/sets/en/ascended-heroes.vue
@@ -5,7 +5,6 @@
             <p>Expected EN Release Date: January 30th</p>
             <p>Cards: 217/295</p>
             <p>Two Reverse Holos for each Pokemon</p>
-            <p class="font-bold">Cards are being added as numbers are confirmed.</p>
         </div>
         <card-list set="en_asc" />
     </div>
